ae5347fc42c7c38d8147a3d895a5cee79cc659e7
LOVE-AMOR <loveamorbalaqui2@gmail.com>
2024-11-04 05:11:52 -0800

    favnumcalculation

--- Favoritenumbercalculation.js
+++ Favoritenumbercalculation.js
@@ -1,13 +1,13 @@
 let favNumber = 7; 
-let guess = prompt("Guess my favorite number:");
+let guess = parseInt(prompt("Guess my favorite number:"));
 
-while (guess != favNumber) {
+while (guess !== favNumber) {
     if (guess < favNumber) {
         console.log("Too low! Try again.");
     } else {
         console.log("Too high! Try again.");
     }
-    guess = prompt("Guess my favorite number:");
+    guess = parseInt(prompt("Guess my favorite number:"));
 }
 
 console.log("You guessed it! My favorite number is " + favNumber);
